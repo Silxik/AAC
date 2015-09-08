d20aa89557284625c2e57faf545ab9259e15619a
Andres Spak <andresspak@gmail.com>
2015-09-08 21:43:07 +0300

    Update .2 php variables

--- session.php
+++ session.php
@@ -1,11 +1,7 @@
 <?php
 include('conn.php');
 if(isset($_SESSION['login_user'])){
-	$user_check = $_SESSION['login_user'];
-	$ses_sql = "SELECT username, profile_image FROM user WHERE username='$user_check'";
-	$query = $conn->query($ses_sql);
-	$row = $query->fetch_assoc();
-	$login_session = $row['username'];
+	$login_session = $_SESSION['login_user'];
 	
 	$inactive = 600;
 	if( !isset($_SESSION['timeout']) )
