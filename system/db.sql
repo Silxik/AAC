9f7be2bbe424f06fe17ba26b098a2a02fbba327d
Andres Spak <andresspak@gmail.com>
2015-09-14 12:01:03 +0300

    MySql update

--- system/db.sql
+++ system/db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Loomise aeg: Sept 10, 2015 kell 07:58 EL
+-- Loomise aeg: Sept 14, 2015 kell 10:51 EL
 -- Serveri versioon: 5.6.24
 -- PHP versioon: 5.5.24
 
@@ -34,33 +34,21 @@ CREATE TABLE IF NOT EXISTS `user` (
   `email` varchar(225) CHARACTER SET utf8 NOT NULL,
   `birthday` date NOT NULL,
   `bio` text CHARACTER SET utf8 NOT NULL,
-  `log` varchar(5) CHARACTER SET utf8 NOT NULL
+  `log` varchar(5) CHARACTER SET utf8 NOT NULL,
+  `location` varchar(200) NOT NULL,
+  `date_joined` date NOT NULL,
+  `gender` tinyint(1) NOT NULL,
+  `ip_joined` varchar(64) NOT NULL,
+  `ip_last` varchar(64) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Andmete tõmmistamine tabelile `user`
 --
 
-INSERT INTO `user` (`user_id`, `username`, `password`, `profile_image`, `email`, `birthday`, `bio`, `log`) VALUES
-(1, 'Test', 'Test', 'pildid/Test_icon_thumb.jpg', '', '0000-00-00', '', 'in'),
-(2, 'Test''2', 'Test', '', '', '0000-00-00', '', 'in');
-
--- --------------------------------------------------------
-
---
--- Tabeli struktuur tabelile `version`
---
-
-CREATE TABLE IF NOT EXISTS `version` (
-  `version_nr` varchar(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Andmete tõmmistamine tabelile `version`
---
-
-INSERT INTO `version` (`version_nr`) VALUES
-('1');
+INSERT INTO `user` (`user_id`, `username`, `password`, `profile_image`, `email`, `birthday`, `bio`, `log`, `location`, `date_joined`, `gender`, `ip_joined`, `ip_last`) VALUES
+(1, 'Test', '', 'pildid/Test_icon_thumb.jpg', '', '0000-00-00', '', 'in', '', '0000-00-00', 0, '', ''),
+(2, 'Test''2', '', '', '', '0000-00-00', '', 'out', '', '0000-00-00', 0, '', '');
 
 --
 -- Indeksid tõmmistatud tabelitele
